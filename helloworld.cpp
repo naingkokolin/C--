456e5379a2c6792dde1e06067b93598922a83c60
naingkokolin <naingkokolin007@gmail.com>
2022-09-13 12:00:42 +0630

    Testing

--- helloworld.cpp
+++ helloworld.cpp
@@ -8,5 +8,9 @@ int main()
     auto var4{23.0l};
     auto var5{"e"};
 
+    std::cout << "hello world!" << std::endl;
+    std::cout << std::showpoint;
+    std::cout << var1 << std::endl;
+
     return 0;
 }
