c7287208a3b2dcb07e753e819e29d1cbc79dedc3
naingkokolin <naingkokolin007@gmail.com>
2022-09-15 12:41:37 +0630

    code testing from day 2 learning

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,16 +1,32 @@
 #include <iostream>
-#include <cmath>
+#include <iomanip>
 
 int main()
 {
-    short int var1{10};
-    short int var2{20};
+    std::string firstName = "naing ko";
+    std::string lastName = "ko lin";
+    int age{21};
 
-    std::cout << "size of var1 : " << sizeof(var1) << std::endl;
-    std::cout << "size of var2 : " << sizeof(var2) << std::endl;
+    std::string firstName1 = "hsu phyu";
+    std::string lastName1 = "sin";
+    int age1{21};
 
-    auto var = var1 + var2;
-    std::cout << "size of var : " << sizeof(var) << std::endl;
+    std::string firstName2 = "ko koe";
+    std::string lastName2 = "baby";
+    int age2{21};
+
+    std::cout << std::setw(10) << std::left << "First Name" << std::setw(10) << std::left << "Last Name " << std::setw(5) << std::left << "Age" << std::endl;
+    std::cout << std::setw(10) << std::left << firstName << std::setw(10) << std::left << lastName << std::setw(5) << std::left << age << std::endl;
+
+    bool isTrue{true};
+
+    std::cout << "It is true or false in boolalpha : " << std::boolalpha << isTrue << std::endl;
+    std::cout << "It is true or false in noboolalpha : " << std::noboolalpha << isTrue << std::endl;
+    std::cout << "-----------------------------------------------------------------------" << std::endl;
+
+    unsigned int num1{10};
+
+    std::cout << std::showpos << num1 << std::endl;
 
     return 0;
 }
